e3ba1ade181ab2499a016b6c5e1ea551b2a9c7d6
Stupid-Faust <350939344@gapps.yrdsb.ca>
2026-06-03 17:25:29 +0000

    Faust find that the current script still lacks, therefore Faust added the ability to check the rating.

--- scrap.py
+++ scrap.py
@@ -6,14 +6,26 @@ url = "https://books.toscrape.com"
 headers = {
     'User-Agent' : 'Mozilla/5.0'
 }
+ratingMap = {
+    'One' : 1,
+    'Two' : 2,
+    'Three' : 3,
+    'Four' : 4,
+    'Five' : 5
+}
 
 soup = BeautifulSoup(requests.get(url, headers = headers).text, "html.parser")
 books = soup.find_all("article", class_ = "product_pod")
+contents = []
 
 for book in books:
     title = book.h3.a["title"]
     price = book.find("p", class_ = "price_color").get_text(strip = True)
+    classes = book.find("p", class_ = "star-rating")
+    rating = classes['class'][1]
+    ratingNum = ratingMap[rating]
 
-    print(title, price)
+    contents.append({'title' : title, 'price' : price, 'rating' : ratingNum})
 
+print(contents)
 
